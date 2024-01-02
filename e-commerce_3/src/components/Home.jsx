f033712f746bbcfec5f9ec1525fb2aadcf048d27
Canaga81 <Canagamemmedov@gmail.com>
2024-01-02 14:37:53 +0400

    Products

--- e-commerce_3/src/components/Home.jsx
+++ e-commerce_3/src/components/Home.jsx
@@ -1,27 +1,57 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../components/Home.css";
 import { Link } from "react-router-dom";
 import HomeProduct from "./Home_Products";
 import { FaEye } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa6";
+import T1_Avif_Image from "../images/T1.avif";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaCartPlus } from "react-icons/fa";
+import Multi_Banner_1_Image_avif from "../images/Multi-Banner-1.avif";
+import Multi_Banner_2_Image_avif from "../images/Multi-Banner-2.avif";
+import Multi_Banner_3_Image_webp from "../images/Multi-Banner-3.webp";
+import Multi_Banner_4_Image_avif from "../images/Multi-Banner-4.avif";
+import Multi_Banner_5_Image_webp from "../images/Multi-Banner-5.webp";
 
 const Home = () => {
+  // Product Category
+
+  const [newProduct, setNewProduct] = useState([]);
+
+  // Trending Product
+
   const [trendingProduct, setTrendingProduct] = useState(HomeProduct);
 
   // Filter of trending Product
 
   const filtercate = (x) => {
     const filterproduct = HomeProduct.filter((curElm) => {
-      return curElm.type === x
-    })
+      return curElm.type === x;
+    });
     setTrendingProduct(filterproduct);
-  }
+  };
 
   // All Trending Product
 
   const allTrendingProduct = () => {
-    setTrendingProduct(HomeProduct)
-  }
+    setTrendingProduct(HomeProduct);
+  };
+
+  // Product Type
+
+  useEffect(() => {
+    productcategory();
+  }, []);
+
+  const productcategory = () => {
+    const newcategory = HomeProduct.filter((x) => {
+      return x.type === "new";
+    });
+    setNewProduct(newcategory);
+  };
 
   return (
     <>
@@ -45,18 +75,18 @@ const Home = () => {
                   <h2 onClick={() => allTrendingProduct()}>Trending Product</h2>
                 </div>
                 <div className="cate">
-                  <h3 onClick={() => filtercate('new')}>New</h3>
-                  <h3 onClick={() => filtercate('featured')}>Featured</h3>
-                  <h3 onClick={() => filtercate('top')}>Top Selling</h3>
+                  <h3 onClick={() => filtercate("new")}>New</h3>
+                  <h3 onClick={() => filtercate("featured")}>Featured</h3>
+                  <h3 onClick={() => filtercate("top")}>Top Selling</h3>
                 </div>
               </div>
 
               <div className="products">
                 <div className="container">
-                  {trendingProduct.map((curElm) => {
+                  {trendingProduct.map((curElm, idx) => {
                     return (
                       <>
-                        <div className="box">
+                        <div className="box" key={idx}>
                           <div className="img_box">
                             <img src={curElm.image} alt="" />
                             <div className="icon">
@@ -79,11 +109,117 @@ const Home = () => {
                     );
                   })}
                 </div>
+                <button className="show_btn">Show More</button>
               </div>
             </div>
 
             <div className="right-box">
-              
+              <div className="right_container">
+                <div className="testimonial">
+                  <div className="head">
+                    <h3>Our Testimonial</h3>
+                  </div>
+                  <div className="detail">
+                    <div className="img_box">
+                      <img src={T1_Avif_Image} alt="testimonial" />
+                    </div>
+                    <div className="info">
+                      <h3>Stephan Robot</h3>
+                      <h4>Web Designer</h4>
+                      <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Odio explicabo temporibus recusandae totam commodi
+                        soluta molestias laboriosam. Voluptate.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="newsletter">
+                  <div className="head">
+                    <h3>Newsletter</h3>
+                  </div>
+                  <div className="form">
+                    <p>join our malling list</p>
+                    <input
+                      type="email"
+                      placeholder="E-mail"
+                      autoComplete="off"
+                    />
+                    <button>Subscribe</button>
+                    <div className="icon_box">
+                      <div className="icon">
+                        <FaFacebookF />
+                      </div>
+                      <div className="icon">
+                        <FaTwitter />
+                      </div>
+                      <div className="icon">
+                        <FaInstagram />
+                      </div>
+                      <div className="icon">
+                        <FaYoutube />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="banners">
+          <div className="container">
+            <div className="left_box">
+              <div className="box">
+                <img src={Multi_Banner_1_Image_avif} alt="" />
+              </div>
+              <div className="box">
+                <img src={Multi_Banner_2_Image_avif} alt="" />
+              </div>
+            </div>
+
+            <div className="right_box">
+              <div className="top">
+                <img src={Multi_Banner_3_Image_webp} alt="" />
+                <img src={Multi_Banner_4_Image_avif} alt="" />
+              </div>
+
+              <div className="bottom">
+                <img src={Multi_Banner_5_Image_webp} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="product_type">
+          <div className="container">
+            <div className="box">
+              <div className="header">
+                <h2>New Product</h2>
+              </div>
+
+              {newProduct.map((curElm, idx) => {
+                return (
+                  <>
+                    <div className="productbox" key={idx}>
+                      <div className="img-box">
+                        <img src={curElm.image} alt="" />
+                      </div>
+
+                      <div className="detail">
+                        <h3>{curElm.Name}</h3>
+                        <p>${curElm.price}</p>
+                        <div className="icon">
+                          <button><FaEye /></button>
+                          <button><FaHeart /></button>
+                          <button><FaCartPlus /></button>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+              })}
             </div>
           </div>
         </div>
--- e-commerce_3/src/components/Home_Products.jsx
+++ e-commerce_3/src/components/Home_Products.jsx
@@ -6,6 +6,10 @@ import Image_P5_Webp from '../images/p5.webp';
 import Image_P6_Webp from '../images/p6.webp';
 import Image_P7_Webp from '../images/p7.webp';
 import Image_P8_Webp from '../images/p8.webp';
+import Image_P9_jpg from '../images/p9.jpg';
+import Image_P10_jpg from '../images/p10.jpg';
+import Image_P11_jpg from '../images/p11.jpg';
+import Image_P12_jpg from '../images/p12.jpg';
 
 const HomeProduct = [
     
@@ -14,7 +18,7 @@ const HomeProduct = [
         Name: 'Product_1',
         price: 33,
         image: Image_P1_Webp,
-        cat: '',
+        cat: 'tv',
         type: 'new',
     },
     {
@@ -22,7 +26,7 @@ const HomeProduct = [
         Name: 'Product_2',
         price: 24,
         image: Image_P2_Webp,
-        cat: '',
+        cat: 'laptop',
         type: 'new',
     },
     {
@@ -30,47 +34,79 @@ const HomeProduct = [
         Name: 'Product_3',
         price: 45,
         image: Image_P3_Webp,
-        cat: '',
+        cat: 'watch',
         type: 'new',
     },
     {
         id: 4,
         Name: 'Product_4',
-        price: 18,
-        image: Image_P4_Webp,
-        cat: '',
-        type: 'featured',
+        price: 45,
+        image: Image_P9_jpg,
+        cat: 'laptop',
+        type: 'new',
     },
     {
         id: 5,
         Name: 'Product_5',
-        price: 54,
-        image: Image_P5_Webp,
-        cat: '',
+        price: 18,
+        image: Image_P4_Webp,
+        cat: 'speaker',
         type: 'featured',
     },
     {
         id: 6,
         Name: 'Product_6',
-        price: 36,
-        image: Image_P6_Webp,
-        cat: '',
-        type: 'top',
+        price: 54,
+        image: Image_P5_Webp,
+        cat: 'electronics',
+        type: 'featured',
     },
     {
         id: 7,
         Name: 'Product_7',
-        price: 66,
-        image: Image_P7_Webp,
-        cat: '',
+        price: 36,
+        image: Image_P6_Webp,
+        cat: 'headphone',
         type: 'top',
     },
     {
         id: 8,
         Name: 'Product_8',
+        price: 66,
+        image: Image_P7_Webp,
+        cat: 'electronics',
+        type: 'top',
+    },
+    {
+        id: 9,
+        Name: 'Product_9',
         price: 69,
         image: Image_P8_Webp,
-        cat: '',
+        cat: 'phone',
+        type: 'top',
+    },
+    {
+        id: 10,
+        Name: 'Product_10',
+        price: 69,
+        image: Image_P10_jpg,
+        cat: 'speaker',
+        type: 'top',
+    },
+    {
+        id: 11,
+        Name: 'Product_11',
+        price: 69,
+        image: Image_P11_jpg,
+        cat: 'speaker',
+        type: 'top',
+    },
+    {
+        id: 12,
+        Name: 'Product_12',
+        price: 69,
+        image: Image_P12_jpg,
+        cat: 'speaker',
         type: 'top',
     },
 
